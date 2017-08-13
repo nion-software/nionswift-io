fa5e2f3180329d0583d3533eaf52c32a3b99103d
Chris Meyer <cmeyer1969@gmail.com>
2017-08-13 16:22:57 -0700

    Reorganize into a package, license, setup.

---
+++ setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""
+To upload to PyPI, PyPI test, or a local server:
+python setup.py bdist_wheel upload -r <server_identifier>
+"""
+
+import setuptools
+import os
+
+setuptools.setup(
+    name="nionswift-io",
+    version="0.0.1",
+    author="Nion Software",
+    author_email="swift@nion.com",
+    description="IO handlers for NionSwift.",
+    url="https://github.com/nion-software/nionswift-io",
+    packages=["nionswift_plugin.DM_IO", "nionswift_plugin.TIFF_IO"],
+    license='GPLv3',
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python :: 3.5",
+    ],
+    include_package_data=True,
+    python_requires='~=3.5',
+)
