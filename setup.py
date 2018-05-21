632e985e746803cbfeb09fde1df046eab915bcd8
Chris Meyer <cmeyer1969@gmail.com>
2018-05-21 15:37:06 -0700

    Versioning 0.13.5. DM timestamp.

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ import os
 
 setuptools.setup(
     name="nionswift-io",
-    version="0.13.4",
+    version="0.13.5",
     author="Nion Software",
     author_email="swift@nion.com",
     description="IO handlers for NionSwift.",
     url="https://github.com/nion-software/nionswift-io",
     packages=["nionswift_plugin.DM_IO", "nionswift_plugin.DM_IO.test", "nionswift_plugin.TIFF_IO", "nionswift_plugin.TIFF_IO.test"],
-    install_requires=["niondata"],
+    install_requires=["niondata>=0.13.1"],
     license='Apache 2.0',
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
